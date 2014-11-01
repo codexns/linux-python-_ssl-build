a975d09b4e1558f5f33bb9c82488087bfef3bed7
Will Bond <wbond@wbond-1204x64.(none)>
2014-11-01 13:29:31 -0400

    Added script for Ubuntu 12.04 x64:

---
+++ ubuntu1204-py3-x64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get install git-core build-essential libssl-dev
+
+wget https://www.python.org/ftp/python/3.3.3/Python-3.3.3.tgz
+tar xvfz Python-3.3.3.tgz
+rm Python-3.3.3.tgz
+
+cd Python-3.3.3/
+./configure
+make
+
+cd ..
+mkdir -p st3_linux_x64/libssl-1.0.0
+cp Python-3.3.3/build/lib.linux-x86_64-3.3/_ssl.cpython-33m.so st3_linux_x64/libssl-1.0.0/
