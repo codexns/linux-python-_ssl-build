8a25429bc08c6d7d4e66cb900c5ee08c970ea687
wbond <will@wbond.net>
2014-11-02 06:02:38 -0500

    Change Ubuntu 12.04 script to make a dir named libssl-1.0.0-versioned

--- ubuntu1204-py3-x32.sh
+++ ubuntu1204-py3-x32.sh
@@ -13,5 +13,5 @@ cd Python-3.3.3/
 make
 
 cd ..
-mkdir -p st3_linux_x32/libssl-1.0.0
-cp Python-3.3.3/build/lib.linux-i686-3.3/_ssl.cpython-33m.so st3_linux_x32/libssl-1.0.0/
+mkdir -p st3_linux_x32/libssl-1.0.0-versioned
+cp Python-3.3.3/build/lib.linux-i686-3.3/_ssl.cpython-33m.so st3_linux_x32/libssl-1.0.0-versioned/
