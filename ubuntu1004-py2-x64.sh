2a445d20bfb75495597bfdacf79684e9cbca150a
wbond <will@wbond.net>
2014-11-01 19:38:03 -0700

    Added script for Python 2 x64 with libssl-0.9.8

---
+++ ubuntu1004-py2-x64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get install git-core build-essential libssl-dev
+
+curl -O https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz
+tar xvfz Python-2.6.9.tgz
+rm Python-2.6.9.tgz
+
+patch -p1 < py2-x64.patch
+
+cd Python-2.6.9/
+./configure
+make
+
+cd ..
+mkdir -p st2_linux_x64/libssl-0.9.8
+cp Python-2.6.9/build/lib.linux-x86_64-2.6/_ssl.so st2_linux_x64/libssl-0.9.8/
