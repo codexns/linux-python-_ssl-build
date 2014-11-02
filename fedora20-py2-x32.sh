a469fae57b3219ae47b164739b75669db463867f
wbond <will@wbond.net>
2014-11-01 22:23:09 -0400

    Added script for Python 2 x32 with libssl-10

---
+++ fedora20-py2-x32.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+sudo yum install git nano openssl-devel patch
+sudo yum groupinstall "Development Tools"
+
+curl -O https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz
+tar xvfz Python-2.6.9.tgz
+rm Python-2.6.9.tgz
+
+patch -p1 < py2-x32.patch
+
+cd Python-2.6.9
+./configure
+make
+
+cd ..
+mkdir -p st2_linux_x32/libssl-10
+cp Python-2.6.9/build/lib.linux-i686-2.6/_ssl.so st2_linux_x32/libssl-10/
+
